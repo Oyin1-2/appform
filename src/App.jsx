337c6659b715658c3942a469383b561a0c9df1b5
Oyin1-2 <oyewoleakeem.tunde@gmail.com>
2025-03-29 22:53:24 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import "./App.css";
-import Layout from "./pages/Layout";
+import LayOut from "./pages/LayOut";
 import Info from "./pages/info";
-import Plan from "./pages/Plan";
+//import Plan from "./pages/Plan";
 import AddOns from "./pages/AddOns";
 import Summary from "./pages/Summary";
 import PageNotFound from "./pages/PageNotFound";
+import Plan from "./pages/plan";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<LayOut />}>
           <Route index element={<Navigate to="info" replace />} />
           <Route path="info" element={<Info />} />
           <Route path="plan" element={<Plan />} />
