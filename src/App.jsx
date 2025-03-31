9c95b78cecba894a904cbdd5c73faadb2db33455
Oyin1-2 <oyewoleakeem.tunde@gmail.com>
2025-03-31 16:18:58 +0100

    nothing to say

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import "./App.css";
 import LayOut from "./pages/LayOut";
-import Info from "./pages/Info";
+import Info from "./pages/info";
 //import Plan from "./pages/Plan";
 import AddOns from "./pages/AddOns";
 import Summary from "./pages/Summary";
 import PageNotFound from "./pages/PageNotFound";
-import Plan from "./pages/plan";
 import NextPage from "./pages/NextPage";
+import Plan from "./pages/plan";
+
 
 function App() {
   return (
@@ -16,11 +17,10 @@ function App() {
         <Route path="/" element={<LayOut />}>
           <Route index element={<Navigate to="info" replace />} />
           <Route path="info" element={<Info />} />
-          <Route path="plan" element={<Plan />} />
+          <Route path="Plan" element={<Plan />} />
           <Route path="add-on" element={<AddOns />} />
           <Route path="summary" element={<Summary />} />
           <Route path="next-page" element={<NextPage />} />
-
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
